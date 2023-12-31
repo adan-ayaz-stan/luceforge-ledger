043c669014269b708f2255622b352f8b418f6987
Spitfire <999a.d.a.n.a.y.a.z999@gmail.com>
2023-12-31 15:16:21 +0500

    Logout button on the dashboard

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="flex justify-center items-center">
     Dashboard: You are logged in. User Email: {{ user?.email }}
+
+    <el-button type="danger" @click="signOut()">Log out</el-button>
   </div>
 </template>
 
 <script lang="ts" setup>
 const user = useSupabaseUser();
+const supabase = useSupabaseClient();
+
+async function signOut() {
+  const { error } = await supabase.auth.signOut();
+  if (!error) {
+    navigateTo("/");
+  }
+}
 </script>
 
 <style></style>
